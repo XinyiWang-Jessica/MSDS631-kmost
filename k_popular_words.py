756c68d066cbbd8d19dcb0fbf71561fc4252ecf6
Johnny <jhnyc@connect.hku.hk>
2023-06-04 18:20:26 -0700

    Add initial implementation of KMostPopularWords class

---
+++ k_popular_words.py
@@ -0,0 +1,128 @@
+
+import string
+from typing import Dict, Generator, List, Tuple
+import os
+import psutil
+from threading import Thread, Lock
+from utils import reset_word_counter, read_file_chunks, split_text_chunks, remove_punctuation
+from multiprocessing import Pool
+
+
+class KMostPopularWords:
+    def __init__(self, file_path: str):
+        # Path of the file to read from
+        self.file_path = file_path
+
+        # Use hash map to keep track of word counts
+        self.word_counter = {}
+
+        # Lock for thread-safe access to variables
+        self.lock = None
+
+        # Load stopwords
+        with open('data/stopwords.txt', 'r') as f:
+            # Use set for fast lookup in constant time
+            self.stopwords = set([line.strip("\n") for line in f.readlines()])
+
+    def count_words(self, text) -> None:
+        for word in text.split():
+            # Convert to lowercase
+            word_lower = word.lower()
+            # Check whether a word is a stop word
+            if not word_lower in self.stopwords:
+                if self.lock:
+                    with self.lock:
+                        self.word_counter[word_lower] = self.word_counter.get(
+                            word_lower, 0) + 1
+                else:
+                    self.word_counter[word_lower] = self.word_counter.get(
+                        word_lower, 0) + 1
+
+    def is_file_size_safe(self) -> bool:
+        """Check if the file size is within a safe limit to be loaded into memory"""
+        file_size = os.path.getsize(self.file_path)
+        memory_usage = psutil.virtual_memory()
+        available_memory = memory_usage.available
+        return file_size < available_memory
+
+    @reset_word_counter
+    def get_top_k_words_baseline(self, k: int) -> List[Tuple[str, int]]:
+        """Naive approach to load the full text into memory and count words"""
+        # Check whether the text can fit into memory
+        if not self.is_file_size_safe():
+            raise ValueError(
+                "File size is too large to be loaded into memory.")
+
+        with open(self.file_path, 'r') as file:
+            text = file.read().replace('\n', ' ')
+            text = remove_punctuation(text)
+            self.count_words(text)
+
+        return sorted(self.word_counter.items(), key=lambda x: x[1], reverse=True)[:k]
+
+    @reset_word_counter
+    def get_top_k_words_chunk(self, k: int, chunk_size: int) -> List[Tuple[str, int]]:
+        """Split text into smaller chunks and count words in each chunk iteratively"""
+        # Iterate over chunks of the text
+        for chunk in read_file_chunks(self.file_path, chunk_size):
+            # Iterate over words of a chunk
+            self.count_words(chunk)
+
+        return sorted(self.word_counter.items(), key=lambda x: x[1], reverse=True)[:k]
+
+    @reset_word_counter
+    def get_top_k_words_chunk_mt(self, k: int, num_threads: int, chunk_size: int) -> List[Tuple[str, int]]:
+        """Apply multithreading to each chunk"""
+        # Lock for thread-safe access to word_counter to ensure that only 1 thread can access the word_counter at a time, preventing potential race conditions
+        self.lock = Lock()
+
+        for chunk in read_file_chunks(self.file_path, chunk_size):
+            # Further split the chunk into sub-chunks to be distributed across multiple threads
+            sub_chunks = split_text_chunks(chunk, num_threads)
+            # Create threads
+            threads = []
+            for _, sub_chunk in zip(range(num_threads), sub_chunks):
+                t = Thread(target=self.count_words, args=(sub_chunk,))
+                t.start()
+                threads.append(t)
+
+            # Wait for all threads to finish
+            for t in threads:
+                t.join()
+
+        # Clean up lock
+        self.lock = None
+        return sorted(self.word_counter.items(), key=lambda x: x[1], reverse=True)[:k]
+
+    def count_words_mp(self, text):
+        """Count word function for multiprocessing"""
+        text = remove_punctuation(text)
+        word_counter = {}
+        for word in text.split():
+            # Convert to lowercase
+            word_lower = word.lower()
+            # Check whether a word is a stop word
+            if not word_lower in self.stopwords:
+                word_counter[word_lower] = word_counter.get(word_lower, 0) + 1
+        return word_counter
+
+    @reset_word_counter
+    def get_top_k_words_chunk_mp(self, k: int, num_processes: int, chunk_size: int) -> List[Tuple[str, int]]:
+        """Apply multiprocessing to each chunk"""
+
+        for chunk in read_file_chunks(self.file_path, chunk_size):
+            # Further split the chunk into sub-chunks to be distributed across multiple threads
+            sub_chunks = list(split_text_chunks(chunk, num_processes))
+            # Create a multiprocessing Pool with the specified number of processes
+            pool = Pool(processes=num_processes)
+
+            # Map the count_words function to process each chunk in parallel
+            results = pool.map(self.count_words_mp, sub_chunks)
+
+            # Merge the results from all processes
+            for result in results:
+                for word, count in result.items():
+                    self.word_counter[word] = self.word_counter.get(
+                        word, 0) + count
+
+        return sorted(self.word_counter.items(), key=lambda x: x[1], reverse=True)[:k]
---
+++ utils.py
@@ -0,0 +1,104 @@
+from functools import wraps
+import time
+import psutil
+import string
+
+
+def reset_word_counter(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Assuming the decorated functions are instance methods of a class
+        self = args[0]
+
+        # Reset the word_counter dictionary
+        self.word_counter = {}
+
+        # Call the original function
+        result = func(*args, **kwargs)
+
+        return result
+
+    return wrapper
+
+def get_complete_words(chunk, remainder):
+    """Extract complete words from a chunk and update the remainder."""
+    # Combine the remainder from the previous chunk with the current chunk
+    chunk = remainder + chunk
+
+    # Find the last occurrence of a space character in the chunk
+    last_space_index = chunk.rfind(' ')
+    if last_space_index != -1:
+        # Extract the complete words from the chunk
+        complete_words = chunk[:last_space_index]
+        # Set the remainder for the next iteration
+        remainder = chunk[last_space_index + 1:]
+    else:
+        complete_words = chunk
+        remainder = ''
+
+    return complete_words, remainder
+
+
+def read_file_chunks(file_path, chunk_size: int):
+    """Read text file in chunks according to the chunk size, ensuring that chunks end on complete words."""
+    with open(file_path, 'r') as file:
+        remainder = ''
+        while True:
+            chunk = file.read(chunk_size).replace("\n", " ")
+            if not chunk:
+                break
+
+            complete_words, remainder = get_complete_words(chunk, remainder)
+            yield complete_words
+
+
+def split_text_chunks(text, num_chunks):
+    """Split string in chunks according to the chunk size, ensuring that chunks end on complete words."""
+    chunk_size = len(text) // num_chunks
+    remainder = ''
+    index = 0
+
+    for _ in range(num_chunks):
+        chunk = text[index:index + chunk_size]
+        index += chunk_size
+
+        complete_words, remainder = get_complete_words(chunk, remainder)
+        yield complete_words
+
+
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+
+
+class ExperimentRunner:
+    def __init__(self):
+        pass
+
+    def measure_resource_utilization(self, start_time: float, process: psutil.Process):
+        """Measure and log the resource utilization."""
+        self.runtime = time.time() - start_time
+        self.cpu_percent = process.cpu_percent()
+        self.memory_usage = process.memory_info().rss
+        print(f"Runtime: {self.runtime:.2f} seconds")
+        print(f"CPU Utilization: {self.cpu_percent:.2f}%")
+        print(f"Memory Usage: {self.memory_usage / 1024 / 1024:.2f} MB")
+
+    def run_experiment(self, version_name: str, func, *args, **kwargs):
+        """Run an experiment and log the results."""
+        print(f"Running experiment: {version_name}")
+
+        # Start time
+        start_time = time.time()
+
+        # Start resource monitoring
+        process = psutil.Process()
+        process.cpu_percent()
+        process.memory_info()
+
+        # Call the function
+        result = func(*args, **kwargs)
+
+        # Stop resource monitoring
+        self.measure_resource_utilization(start_time, process)
+
+        return result
